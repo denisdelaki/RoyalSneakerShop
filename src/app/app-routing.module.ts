1dcb9accf0c3fa68cbb573de2fa2bd21ea76e945
denisdelaki <denisdelaki@gmail.com>
2024-03-24 08:30:33 +0300

    Configured the Module paths and added components to the Authentication module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./authentication/authentication.module')
+        .then(m => m.AuthenticationModule)
+  },
+  { path: 'features', loadChildren: () => import('./features/features.module')
+  .then(m => m.FeaturesModule)
+  },
+  { path: 'pages', loadChildren: () => import('./pages/pages.module')
+  .then(m => m.PagesModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
