c532b701ae6f8e0d4597933a849235ff470d1da3
denisdelaki <denisdelaki@gmail.com>
2024-03-26 16:39:35 +0300

    added paginator

--- src/app/pages/material/material.module.ts
+++ src/app/pages/material/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {  MatIconModule } from '@angular/material/icon';
-
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator'
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatIconModule
+    CommonModule, MatIconModule,MatPaginatorModule,MatPaginator
+
   ]
 })
 export class MaterialModule { }
