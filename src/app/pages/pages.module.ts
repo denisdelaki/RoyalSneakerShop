c578a5608125321da2e04def1f1ab9f0a5758a7f
denisdelaki <denisdelaki@gmail.com>
2024-03-29 12:49:23 +0300

    created the product service to handle services

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Components/home/home.component';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthenticationModule } from '../authentication/authentication.module';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
 import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
