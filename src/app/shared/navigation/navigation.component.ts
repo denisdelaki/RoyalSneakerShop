089d5526888ae088adb51579fc7c601c0e6e0115
denisdelaki <denisdelaki@gmail.com>
2024-03-29 12:14:53 +0300

    added logout to the navigation only visible on successful login

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -6,13 +7,29 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
-  @Output() isSignup: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  @Output() isSignup: EventEmitter<boolean> = new EventEmitter<boolean>();
+  isLoggedIn: boolean = false;
+  constructor(private router: Router) {
+    // Check if there's a user ID in local storage upon component initialization
+    const userId = localStorage.getItem('userId');
+    if (userId) {
+        // If user ID exists, set isLoggedIn to true
+        this.isLoggedIn = true;
+    }
+}
   login() {
     this.isSignup.emit(false);
+    this.isLoggedIn = true;
   }
 
   signup() {
     this.isSignup.emit(true);
+    this.isLoggedIn = true;
+  }
+  logout() {
+    localStorage.removeItem('userId');
+    this.isLoggedIn = false;
+    this.router.navigate(['/']);
   }
 }
